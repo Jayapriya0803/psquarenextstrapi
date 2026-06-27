44c3173489e978a69365fb7403dd2f3602ed5559
PriyaLokalads <priyalokalads@gmail.com>
2026-06-27 18:49:33 +0530

    feat: add investor-inquiry content type

---
+++ src/api/investor-inquiry/controllers/investor-inquiry.ts
@@ -0,0 +1,7 @@
+/**
+ * investor-inquiry controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::investor-inquiry.investor-inquiry');
---
+++ src/api/investor-inquiry/services/investor-inquiry.ts
@@ -0,0 +1,7 @@
+/**
+ * investor-inquiry service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::investor-inquiry.investor-inquiry');
